243c9c19242b69d18468547769065afeca413be9
Jason <jason@Jasons-MacBook-Pro.local>
2014-11-25 10:20:11 -0600

    Require all the other files for the module in the main source file.

--- lib/tictactoe_j8th.rb
+++ lib/tictactoe_j8th.rb
@@ -1,5 +1,10 @@
 require "tictactoe_j8th/version"
 
+require "tictactoe_j8th/ai"
+require "tictactoe_j8th/board"
+require "tictactoe_j8th/game"
+require "tictactoe_j8th/human"
+
 module TictactoeJ8th
   # Any extra module methods here, of which there are none now.
 end
